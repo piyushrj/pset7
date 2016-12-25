ff5a63d459c8cf7ea4011f32b07a932495b45c0c
Piyush-sopho <raj.piyushn72@gmail.com>
2016-12-25 20:01:01 +0000

    Added quote.php as controller and quote_form.php and quote.php(in views) as view to quote the price of a share

---
+++ views/quote.php
@@ -0,0 +1,3 @@
+<?php
+    $fprice=number_format($price, 2, '.','');
+    print("A share of {$name} ({$symbol}) costs <b>\${$fprice}</b>"); ?>
---
+++ views/quote_form.php
@@ -0,0 +1,13 @@
+<form action="quote.php" method="post">
+    <fieldset>
+        <div class="form-gorup">
+            <input autocomplete="off" autofocus class="form-control" name="symbol" placeholder="Symbol" type="text"/>
+        </div>
+        </br>
+        <div class="form-group">
+            <button class="btn btn-default" type="submit">Get Quote</button>
+        </div>
+    </fieldset>
+</form>
+
+
