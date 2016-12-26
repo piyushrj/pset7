e47c6145016e09e04dd691192f9e91344a04c161
Piyush-sopho <raj.piyushn72@gmail.com>
2016-12-26 01:16:24 +0000

    modified index.php in public and portfolio.php in view to present the portfolio to the user on logging in

--- public/index.php
+++ public/index.php
@@ -2,8 +2,24 @@
 
     // configuration
     require("../includes/config.php"); 
-
-    // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    
+    $rows=CS50::query("SELECT portfolios.symbol, portfolios.shares, users.cash FROM portfolios, users WHERE users.id=? AND portfolios.user_id=users.id",$_SESSION["id"]);
+    $positions = [];
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+            "name" => $stock["name"],
+            "price" => $stock["price"],
+            "shares" => $row["shares"],
+            "symbol" => $row["symbol"],
+            "cash" => $row["cash"]
+            ];
+        }
+    }
+    
+    render("portfolio.php", ["title" => "Portfolio", "positions"=>$positions]);
 
 ?>
